241778decde1e31b903b71ff739be330619cd7ed
M4rteaga <m4rteagag@gmail.com>
2022-09-02 17:01:15 -0500

    add contentId parameter to createEntry function

--- contenfulConfig.ts
+++ contenfulConfig.ts
@@ -1,64 +1,62 @@
 import { CreateAssetProps, createClient, Entry } from 'contentful-management';
 import { Movie } from './types';
 
-
-const CONTENT_MANAGEMENT_TOKEN = "CFPAT-0ZSN-nSoykJHPVQ2bAt6fGbhFq2OHfb3m5aOFQhck7c";
-const SPACE_ID = "z8yfizru5lml";
+const CONTENT_MANAGEMENT_TOKEN = 'content-managment-token';
+const SPACE_ID = 'space-id';
 // const BASE_URL = `https://cdn.contentful.com/spaces/${SPACE_ID}`;
 
 const client = createClient({
-  accessToken: CONTENT_MANAGEMENT_TOKEN
-})
-
-
+	accessToken: CONTENT_MANAGEMENT_TOKEN,
+});
 
 const ContentfulHelper = {
-  client,
-  environment: (async () => {
-    const space = await client.getSpace(SPACE_ID);
-    return space.getEnvironment('master');
-  })(),
-  async retriveEntry() {
-    try {
-      const entry = await this.environment.then(e => e.getEntries());
-      return entry;
-    } catch (e) {
-      throw new Error('error leyendo los entry:' + e);
-    }
-  },
+	client,
+	environment: (async () => {
+		const space = await client.getSpace(SPACE_ID);
+		return space.getEnvironment('master');
+	})(),
+	async retriveEntry() {
+		try {
+			const entry = await this.environment.then((e) => e.getEntries());
+			return entry;
+		} catch (e) {
+			throw new Error('error leyendo los entry:' + e);
+		}
+	},
 
-  async createEntry(movieData: Movie) {
-    try {
-      const entry = await this.environment.then(e => e.createEntry('pelicula', { fields: movieData }));
-      return entry
-    } catch (e) {
-      throw new Error('error creating movie entry:' + e);
+	async createEntry(movieData: Movie, contentId: string) {
+		try {
+			const entry = await this.environment.then((e) =>
+				e.createEntry(contentId, { fields: movieData })
+			);
+			return entry;
+		} catch (e) {
+			throw new Error('error creating movie entry:' + e);
+		}
+	},
 
-    }
-  },
+	async publishEntry(entry: Entry) {
+		try {
+			return await entry.publish();
+		} catch (e) {
+			throw new Error('error publishing movie entry:' + e);
+		}
+	},
 
-  async publishEntry(entry: Entry) {
-    try {
-      return await entry.publish();
-    } catch (e) {
-      throw new Error('error publishing movie entry:' + e);
-    }
-  },
+	async retriveAllAssets() {
+		return await this.environment.then((e) => e.getAssets());
+	},
 
-  async retriveAllAssets() {
-    return await this.environment.then(e => e.getAssets());
-  },
-
-  async createAsset(asset: CreateAssetProps) {
-    try {
-      const assetCreated = await this.environment.then(e => e.createAsset(asset))
-      return await assetCreated.processForAllLocales().then(a => a.publish());
-    } catch (err) {
-      throw new Error('error creating asset ' + err)
-    }
-  }
-
-}
-
+	async createAsset(asset: CreateAssetProps) {
+		try {
+			const assetCreated = await this.environment.then((e) =>
+				e.createAsset(asset)
+			);
+			return await assetCreated.processForAllLocales().then((a) => a.publish());
+		} catch (err) {
+			throw new Error('error creating asset ' + err);
+		}
+	},
+};
 
 export const contentfulHelper = Object.freeze(ContentfulHelper);
--- index.ts
+++ index.ts
@@ -1,103 +1,114 @@
-import { CreateAssetProps, createClient, Entry } from 'contentful-management'
+import { CreateAssetProps, createClient, Entry } from 'contentful-management';
 import { contentfulHelper } from './contenfulConfig';
 import { Movie } from './types';
 
-// 
+//
 const movieSample: Movie = {
-  name: {
-    "en-US": 'pelicula 4'
-  },
-  adult: {
-    "en-US": false,
-  },
-  description: {
-    "en-US": {
-      data: {},
-      nodeType: 'document',
-      content: [
-        {
-          data: {},
-          content: [{
-            data: {},
-            marks: [],
-            nodeType: 'text',
-            value: 'la cuarta mejor pleli con imagen'
-          }],
-          nodeType: 'paragraph',
-        }
-      ]
-    }
-  },
-  duration: {
-    "en-US": 'toda una vida'
-  },
-  price: {
-    "en-US": 200
-  },
-  //reference
-  genres: {
-    "en-US": [
-      {
-        sys: {
-          id: '4zyKVFGO67D9ZnRjgz8gee',
-          linkType: 'Entry',
-          type: 'Link'
-        }
-      }
-    ]
-  },
-  //asset ref
-  image: {
-    "en-US": {
-      sys: {
-        id: '3uFFTTFIsuWWbfuXcxOd9v',
-        linkType: 'Asset',
-        type: 'Link'
-      }
-    }
-  }
-}
-//ejemplo genero miedo
+	name: {
+		'en-US': 'pelicula 4',
+	},
+	adult: {
+		'en-US': false,
+	},
+	description: {
+		'en-US': {
+			data: {},
+			nodeType: 'document',
+			content: [
+				{
+					data: {},
+					content: [
+						{
+							data: {},
+							marks: [],
+							nodeType: 'text',
+							value: 'la cuarta mejor pleli con imagen',
+						},
+					],
+					nodeType: 'paragraph',
+				},
+			],
+		},
+	},
+	duration: {
+		'en-US': 'toda una vida',
+	},
+	price: {
+		'en-US': 200,
+	},
+	//reference
+	genres: {
+		'en-US': [
+			{
+				sys: {
+					id: '4zyKVFGO67D9ZnRjgz8gee',
+					linkType: 'Entry',
+					type: 'Link',
+				},
+			},
+		],
+	},
+	//asset ref
+	image: {
+		'en-US': {
+			sys: {
+				id: '3uFFTTFIsuWWbfuXcxOd9v',
+				linkType: 'Asset',
+				type: 'Link',
+			},
+		},
+	},
+};
+//example of a link entry
 //{ type: 'Link', linkType: 'Entry', id: '4zyKVFGO67D9ZnRjgz8gee' }
-//ejemplo linkear asset
+//example of a link asset
 //{ type: 'Link', linkType: 'Asset', id: '2nJlVwULBQjsbTQSfVk7dj' }
 
 //ejemplo creacion de imagen
 const asset1: CreateAssetProps = {
-  fields: {
-    title: {
-      'en-US': 'asset 1'
-    },
-    description: {
-      'en-US': 'first example of asset upload by content managment api'
-    },
-    file: {
-      'en-US': {
-        contentType: 'image/webp',
-        fileName: 'my-first-image-test.webp',
-        upload: 'https://occ-0-1595-3933.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABZcQsRT8h64hXXVbJvotmH4p2vb-FsxDVcCrGt98u1R2fLr1FpSSoRf3koKTtXzclvyUFZDYatHpRY5R7HzxGTPsDQ6zPCdSWeY5.webp?r=c6a'
-      }
-    }
-  }
-}
+	fields: {
+		title: {
+			'en-US': 'asset 1',
+		},
+		description: {
+			'en-US': 'first example of asset upload by content managment api',
+		},
+		file: {
+			'en-US': {
+				contentType: 'image/webp',
+				fileName: 'my-first-image-test.webp',
+				upload:
+					'https://occ-0-1595-3933.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABZcQsRT8h64hXXVbJvotmH4p2vb-FsxDVcCrGt98u1R2fLr1FpSSoRf3koKTtXzclvyUFZDYatHpRY5R7HzxGTPsDQ6zPCdSWeY5.webp?r=c6a',
+			},
+		},
+	},
+};
 
+function treatImgageUrl(imageUrl: string) {
+	const tmpUrl = new URL(imageUrl);
+	const path = tmpUrl.pathname;
+	return path.split('/').pop()?.split('.').pop();
+}
 
 function main() {
-  //create and publish an entry
-  contentfulHelper.createEntry(movieSample).then(async (e) => {
-    const res = await contentfulHelper.publishEntry(e);
-    console.log(res);
-  });
-  // return all entries
-  contentfulHelper.
-    retriveEntry().then((entry) => entry.items.forEach((i) => {
-      console.log(i.fields);
-    }));
-  //return all assets of a space
-  // contentfulHelper.getAllAssets()
-  //   .then(assets => assets.items.map(a => console.log(a.fields.file)));
-  //create asset
-  // contentfulHelper.createAsset(asset1).then(a => console.log(a));
+	//create and publish an entry
+	// contentfulHelper.createEntry(movieSample,'pelicula').then(async (e) => {
+	//   const res = await contentfulHelper.publishEntry(e);
+	//   console.log(res);
+	// });
+	// return all entries
+	contentfulHelper.retriveEntry().then((entry) =>
+		entry.items.forEach((i) => {
+			console.log(i.sys.id);
+		})
+	);
+	//return all assets of a space
+	// contentfulHelper.getAllAssets()
+	//   .then(assets => assets.items.map(a => console.log(a.fields.file)));
+	//create asset
+	// contentfulHelper.createAsset(asset1).then(a => console.log(a));
+	// const imageName = treatImgageUrl("https://occ-0-1456-3933.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABW9UxuELV8JI0rY7QqQhsPhJvMyJsG0syQryPQAVXwtPMkNKYH9GqwOCZCVemX9t2z_22BFui9rhanCnEEkRz0wV2u6nfvbHu4A2.jpg?r=f9c");
+	// console.log(imageName);
 }
 
-main()
+main();
